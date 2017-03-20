e3178cec6a41bacb38cc98fe39289f6c8ed2d17b
R Bakst <R Bakst>
2017-03-20 16:44:49 -0400

    initial upload of project

--- SalesProject/Program.cs
+++ SalesProject/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace SalesProject
 {
-    class Program
+    public class Sale
     {
-        static void Main(string[] args)
+        public String Item { get; set; }
+        public String Customer { get; set; }
+        public double PricePerItem { get; set; }
+        public int Quantity { get; set; }
+        public String Address { get; set; }
+        public bool ExpeditedShipping { get; set; }
+
+        public Sale(String item, string customer, double price, int quant, string addr, bool expedited)
         {
+            Item = item;
+            Customer = customer;
+            PricePerItem = price;
+            Quantity = quant;
+            Address = addr;
+            ExpeditedShipping = expedited;
         }
     }
 }
